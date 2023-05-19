ea0dab638391a5238cb1a4c7862789138eae32d5
Uilton Rodrigues <uiltondeoliveira@gmail.com>
2023-05-19 16:59:45 -0300

    Alterações no teste automatizado

---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade: Página de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit("http://lojaebac.ebaconline.art.br/produtos/")
+    });
+
+    it('Deve selecionar um produto', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 10
+
+        cy.get('.product-block')
+            .contains('Abominable Hoodie').click()
+
+            cy.get('.button-variable-item-M').click()
+            cy.get('.button-variable-item-Red').click()
+            cy.get('.input-text').clear().type(quantidade)
+            cy.get('.single_add_to_cart_button').click()
+
+            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+            cy.get('.woocommerce-message').should('contain', quantidade + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+
+            
+
+
+
+
+    });
+});
